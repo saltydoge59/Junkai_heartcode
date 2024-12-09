cbd57afcbe9ec3f264cde48bfe8a0f8981eeb7d6
JunKai627 <junkai627@gmail.com>
2024-12-09 15:36:25 +0800

    changed landing page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="text-red-900 text-6xl bg-purple-200">
       Hello!
       <div className = "flex flex-row justify-center text-blue-300">
-        aosjdaod
+        aosjdaodwoefewnf
       </div>
     </div>
   );
