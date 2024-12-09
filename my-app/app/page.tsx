69e740fe2f164c6d9f57d38848138f2a0e727307
JunKai627 <junkai627@gmail.com>
2024-12-09 15:34:11 +0800

    my first commit!

---
+++ my-app/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <div className="text-red-900 text-6xl bg-purple-200">
+      Hello!
+      <div className = "flex flex-row justify-center text-blue-300">
+        aosjdaod
+      </div>
+    </div>
+  );
+}
